50ced18e9e5b7dad523db16e37468a3c335ea1e9
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2026-07-06 01:21:22 +0800

    feat: update member data schema

--- apps/web/src/content.config.ts
+++ apps/web/src/content.config.ts
@@ -2,8 +2,31 @@ import { file, glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 import { defineCollection, reference } from 'astro:content';
 
-import { GroupKeys } from './groups';
-import { IdentitiesKeys } from './identities';
+export enum MemberStatus {
+  ACTIVE = '在职',
+  INACTIVE = '暂离',
+  ALUMNI = '离开',
+}
+
+export const MemberStatusKeys = Object.keys(MemberStatus) as (keyof typeof MemberStatus)[];
+
+const members = defineCollection({
+  loader: file('./contents/members.json'),
+  schema: z.object({
+    // 个人信息
+    id: z.string(), // Github 用户名，即个人主页中的 URL 中的最后一段
+    nickname: z.string().optional(), // 昵称，为空时使用 id 作为昵称
+    url: z.url().optional(), // 个人主页的 URL，为空时使用 Github 主页作为个人主页
+
+    title: z.string(), // 贡献职责称谓，为空时使用默认值
+    description: z.string(), // 贡献边界说明
+    tags: z.array(z.string()).default([]), // 贡献边界标签
+    status: z.enum(MemberStatusKeys).default('ACTIVE'), //贡献状态
+    join_time: z.iso.date().optional(), // 加入时间
+    leave_time: z.iso.date().optional(), // 离开时间
+    sort: z.number().int().optional(), // 排序值，越小越靠前，为空时使用 id 排序
+  }),
+});
 
 const blogs = defineCollection({
   loader: glob({
@@ -47,40 +70,9 @@ const pages = defineCollection({
   }),
 });
 
-const members = defineCollection({
-  loader: file('./contents/members.json'),
-  schema: z.object({
-    id: z.string(), // GitHub 用户名，用于获取头像和Github（个人页面中 URL 地址显示的用户名）
-    description: z.string(), // 个人简介
-    group: z.array(z.enum(GroupKeys as [string])), // 身份组
-    identity: z.enum(IdentitiesKeys as [string]), // 身份标识
-    url: z.url().optional(), // 个人网站网址
-    nickname: z.string().optional(), // 昵称，如果不填写则使用 GitHub 用户名
-    status: z.boolean().default(true), // true 为在职，false 为离开，默认为 true
-    join_time: z
-      .string()
-      .transform((str) => new Date(str))
-      .optional(), // 加入时间
-    leave_time: z
-      .string()
-      .transform((str) => new Date(str))
-      .optional(), // 离开时间
-  }),
-});
-
-const group = defineCollection({
-  loader: file('./contents/groups.json'),
-  schema: z.object({
-    id: z.enum(GroupKeys as [string]), // 组唯一标识符
-    name: z.string(), // 组名称
-    description: z.string(), // 组描述
-  }),
-});
-
 export const collections = {
   blogs,
   docs,
   pages,
   members,
-  group,
 };
