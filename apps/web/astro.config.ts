29de2a72fe4f4901472624514f9151be3f84873a
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2026-07-21 11:20:35 +0800

    feat: add site configuration

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,13 +1,21 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import { siteConfig } from './heyblog.config';
+
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone',
+  }),
+  output: 'server',
+  site: siteConfig.url,
   vite: {
     plugins: [tailwindcss()],
   },
